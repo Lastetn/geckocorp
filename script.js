ef5d62e477a0c7b4da6eb25f8b9c4c1ac3430c2e
Lastetn <luther.leloup@outlook.com>
2026-06-25 14:36:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ toggle.addEventListener("click", () => {
 
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const supabaseUrl = "https://frtvvqdvdwjzrxnvcrgy.supabase.co/rest/v1/";
+const supabaseUrl = "https://frtvvqdvdwjzrxnvcrgy.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZydHZ2cWR2ZHdqenJ4bnZjcmd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzODExNTYsImV4cCI6MjA5Nzk1NzE1Nn0.ip-Gx4OjjEAAvCDdNjJXLonUAtaEll5nUJRalaYh6Cs";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
