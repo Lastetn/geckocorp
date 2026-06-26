ff3f2240816abbbe840776db7fb2f217f74fd7d0
Lastetn <luther.leloup@outlook.com>
2026-06-26 13:45:57 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -6,36 +6,3 @@ export const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXB
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
-const supabaseUrl = "https://frtvvqdvdwjzrxnvcrgy.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZydHZ2cWR2ZHdqenJ4bnZjcmd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzODExNTYsImV4cCI6MjA5Nzk1NzE1Nn0.ip-Gx4OjjEAAvCDdNjJXLonUAtaEll5nUJRalaYh6Cs";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-// Afficher / cacher Connexion / Déconnexion
-async function updateAuthUI() {
-    const { data } = await supabase.auth.getUser();
-    const loginLink = document.getElementById("login-link");
-    const logoutLink = document.getElementById("logout-link");
-
-    if (data.user) {
-        loginLink.style.display = "none";
-        logoutLink.style.display = "inline";
-    } else {
-        loginLink.style.display = "inline";
-        logoutLink.style.display = "none";
-    }
-}
-
-updateAuthUI();
-
-// Déconnexion
-document.getElementById("logout-link")?.addEventListener("click", async () => {
-    await supabase.auth.signOut();
-    window.location.reload();
-});
-
-supabase
-  .channel('messages')
-  .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'messages' }, payload => {
-    loadMessages();
-  })
-  .subscribe();
