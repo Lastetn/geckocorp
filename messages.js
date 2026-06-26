3122134e1223ab156a1c673ed80cab13128a1359
Lastetn <luther.leloup@outlook.com>
2026-06-26 18:57:34 +0200

    Update messages.js

--- messages.js
+++ messages.js
@@ -3,10 +3,11 @@
 //  CONFIG SUPABASE
 // ===============================
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
 const supabaseUrl = "https://frtvvqdvdwjzrxnvcrgy.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZydHZ2cWR2ZHdqenJ4bnZjcmd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzODExNTYsImV4cCI6MjA5Nzk1NzE1Nn0.ip-Gx4OjjEAAvCDdNjJXLonUAtaEll5nUJRalaYh6Cs";
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
+const supabase = createClient(supabaseUrl, supabaseKey);
 // ===============================
 //  CHARGER LES MESSAGES
 // ===============================
