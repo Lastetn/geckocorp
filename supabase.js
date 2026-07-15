0ee650a99967ee9dbde8bdcccf51d0aeabd5541c
Lastetn <luther.leloup@outlook.com>
2026-07-15 15:01:58 +0200

    Change Supabase URL and key
    Updated Supabase URL and key for the project.

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
 
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-export const supabaseUrl = "https://frtvvqdvdwjzrxnvcrgy.supabase.co";
-export const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZydHZ2cWR2ZHdqenJ4bnZjcmd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzODExNTYsImV4cCI6MjA5Nzk1NzE1Nn0.ip-Gx4OjjEAAvCDdNjJXLonUAtaEll5nUJRalaYh6Cs";
+export const supabaseUrl = "https://ljhizncikukcddsbkdxv.supabase.co";
+export const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqaGl6bmNpa3VrY2Rkc2JrZHh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2OTU4MzMsImV4cCI6MjA5OTI3MTgzM30.zHWJk4AM9ODijSJWMebk0YD565IX82ShmVKwsqoGidg";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
